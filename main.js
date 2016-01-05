109ac3fda4d9a96924a885a9fb637b63c832c28f
Rey Dhuny <rey@me.com>
2016-01-05 19:22:57 +0000

    Now works when everything is in one folder

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var menubar = require('menubar')
 var ping = require('net-ping')
 
+console.log(__dirname);
+
 var mb = menubar(
   {
-    // icon: "images/icon.png",
+    icon: "images/icon.png"
   }
 )
-
-
 
 mb.on('ready', function() {
 
@@ -28,13 +28,14 @@ mb.on('ready', function() {
       // error
       if (error) {
         console.log (target + ": " + error.toString ());
-        // mb.tray.setImage("images/dead.png");
+        mb.tray.setImage("images/dead.png");
       }
 
       // success
       else  {
         console.log (target + " delta: " + delta + " sent: " + sent.getUTCMilliseconds() + " rcvd: " + rcvd.getUTCMilliseconds());
-        // mb.tray.setImage("images/alive.png");
+        mb.tray.setImage("images/alive.png");
+        console.log
       }
     });
 
