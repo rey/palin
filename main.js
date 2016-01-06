05c08142291cb656b3b8b60b88283d22d6dea23a
Rey Dhuny <rey@me.com>
2016-01-06 21:26:42 +0000

    Work in progress

--- main.js
+++ main.js
@@ -1,28 +1,64 @@
-'use strict';
-
-var menubar = require("menubar")
+var app = require('app');
+var Tray = require('tray');
+var Menu = require('menu');
+var path = require('path');
 var ping = require("net-ping")
+var BrowserWindow = require('browser-window');
 
-var mb = menubar(
-  {
-    dir: __dirname + "/app",
-    icon: __dirname + "/app/images/icon.png"
-  }
-)
+var iconPath = path.join(__dirname, 'icon.png');
+var appIcon = null;
+var win = null;
 
+app.on('ready', function(){
+  win = new BrowserWindow({show: false});
+  appIcon = new Tray(iconPath);
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Item1',
+      type: 'radio',
+      icon: iconPath
+    },
+    {
+      label: 'Item2',
+      submenu: [
+        { label: 'submenu1' },
+        { label: 'submenu2' }
+      ]
+    },
+    {
+      label: 'Item3',
+      type: 'radio',
+      checked: true
+    },
+    {
+      label: 'Toggle DevTools',
+      accelerator: 'Alt+Command+I',
+      click: function() {
+        win.show();
+        win.toggleDevTools();
+      }
+    },
+    { label: 'Quit',
+      accelerator: 'Command+Q',
+      selector: 'terminate:',
+    }
+  ]);
+  appIcon.setToolTip('This is my application.');
+  appIcon.setContextMenu(contextMenu);
 
-mb.on("ready", function() {
-
-  console.log("app is ready");
-  console.log(__dirname);
+  var count = 0;
 
   setInterval(function() {
 
-    // var target="8.8.8.8";
-    var target="127.0.0.1";
+    count++;
+
+    var target="8.8.8.8";
+    // var target="127.0.0.1";
     var options = {
-      timeout: 2000
+      timeout: 500
+      // timeout: 2000
     };
+
 
     var session = ping.createSession (options);
     session.pingHost (target, function (error, target, sent, rcvd) {
@@ -31,20 +67,19 @@ mb.on("ready", function() {
 
       // error
       if (error) {
-        console.log (target + ": " + error.toString ());
-        mb.tray.setImage(__dirname + "/app/images/dead.png");
+        console.log ("🍎  " + target + " count: " + count + " error: " + error.toString ());
+        appIcon.setImage(__dirname + "/dead.png");
       }
 
       // success
       else  {
-        console.log (target + " delta: " + delta + " sent: " + sent.getUTCMilliseconds() + " rcvd: " + rcvd.getUTCMilliseconds());
-        mb.tray.setImage(__dirname + "/app/images/alive.png");
+        console.log ("🍏  " + target + " count: " + count +  " delta: " + delta + " sent: " + sent.getUTCMilliseconds() + " rcvd: " + rcvd.getUTCMilliseconds());
+        appIcon.setImage(__dirname + "/alive.png");
       }
+
     });
 
   }, 1000);
+
 });
 
-// mb.on("after-create-window", function() {
-//   mb.window.openDevTools();
-// });
