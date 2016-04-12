6cbb40b703b68a88bf0085bd663fe9e4fa61323a
Rey Dhuny <rey@me.com>
2016-04-12 20:22:43 +0100

    Remove debugging lines, add `getName()`

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ palin.on("ready", function(){
   var appIcon = new Tray(__dirname + "/images/menu/standby@2x.png");
   var contextMenu = Menu.buildFromTemplate([
     {
-      label: "Palin " + palin.getVersion(),
+      label: palin.getName() + " " + palin.getVersion(),
       role: "help",
     },
     {
@@ -25,21 +25,16 @@ palin.on("ready", function(){
 
   setInterval(function() {
     var session = Ping.createSession (options);
-    session.pingHost (target, function (error, target, sent, rcvd) {
-
-      // Calculate a rough ping time
-      var delta = rcvd - sent;
+    session.pingHost (target, function (error, target) {
 
       // If ping isn"t successful
       if (error) {
-        // console.log ("🍎  " + target + " count: " + count + " error: " + error.toString ());
         appIcon.setToolTip("Internet is unavailable");
         appIcon.setImage(__dirname + "/images/menu/dead@2x.png");
       }
 
       // Ping is successful!
       else  {
-        // console.log ("🍏  " + target + " count: " + count +  " delta: " + delta + " sent: " + sent.getUTCMilliseconds() + " rcvd: " + rcvd.getUTCMilliseconds());
         appIcon.setToolTip("Internet is available");
         appIcon.setImage(__dirname + "/images/menu/alive@2x.png");
       }
